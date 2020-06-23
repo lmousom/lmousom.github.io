739469c406a54dc859dc5390a7aa5a4f50bcfcd4
Latiful Mousom <Latifulmousom@gmail.com>
2020-06-23 19:52:54 +0530

    Update app.js

--- pwdg/app.js
+++ pwdg/app.js
@@ -91,7 +91,6 @@ function getRandomSymbol() {
 var canvasDiv = document.getElementById('particle-canvas');
 var options = {
   particleColor: '#888',
-  background: 'https://raw.githubusercontent.com/JulianLaval/canvas-particle-network/master/img/demo-bg.jpg',
   interactive: true,
   speed: 'medium',
   density: 'high'
