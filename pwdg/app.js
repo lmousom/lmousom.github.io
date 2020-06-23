907d75332b6cbe89379f652c8148e337d50b96f5
Latiful Mousom <Latifulmousom@gmail.com>
2020-06-23 19:42:43 +0530

    Update app.js

--- pwdg/app.js
+++ pwdg/app.js
@@ -78,3 +78,15 @@ function getRandomSymbol() {
 	const symbols = '!@#$%^&*(){}[]=<>/,.'
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+// particle.min.js hosted on GitHub (https://github.com/JulianLaval/canvas-particle-network)
+
+var canvasDiv = document.getElementById('particle-canvas');
+var options = {
+  particleColor: '#e37575',
+ 
+  interactive: true,
+  speed: 'medium',
+  density: 'high'
+};
+var particleCanvas = new ParticleNetwork(canvasDiv, options);
