1bdd65a4a5f315a97da5badcae28bef4ae048642
Latiful Mousom <Latifulmousom@gmail.com>
2020-09-01 20:53:09 +0530

    Update site.js

--- exam/src/js/site.js
+++ exam/src/js/site.js
@@ -112,7 +112,6 @@ if(points <= 0){
   sessionStorage.setItem("points", points);
 
 }
-if(time > 60){location.href = "https://mousom.com/exam/end.html";}
 
 function show(count) {
   let question = document.getElementById("questions");
