65a7ee2696c50dd168ec3e6c27b22290896cb4c4
Latiful Mousom <Latifulmousom@gmail.com>
2020-09-01 11:09:54 +0530

    Update userInfo.js

--- exam/src/js/userInfo.js
+++ exam/src/js/userInfo.js
@@ -3,13 +3,10 @@ let user_points = sessionStorage.getItem("points");
 let user_time = sessionStorage.getItem("time");
 var usr_time = parseInt(user_time/60);
 var sec = user_time % 60;
+document.querySelector("span.points").innerHTML = user_points;
 if(user_time < 60){
   document.querySelector("span.time_taken").innerHTML = user_time;
 }
 else{
  document.querySelector("span.time_taken").innerHTML = usr_time + " min " + sec;
 }
-document.querySelector("span.name").innerHTML = user_name;
-document.querySelector("span.points").innerHTML = user_points;
-
-
